c53abd9783f0fd63779b1acf41ef7a04d989e03a
MadhubalaRaju <119468423+MadhubalaRaju@users.noreply.github.com>
2022-11-30 23:49:14 +0800

    Sum of n positive integers

---
+++ Prog2.py
@@ -0,0 +1,9 @@
+Write a program to print the sum of n positive integers (1 to n), where n is input by the user.
+
+n = int(input("Enter the number: "))
+
+#sum = (n)(n+1)/2 
+
+sum = ((n)*(n+1))/2 
+
+print(sum)
